0aadbd2b9cf97bfe317e003206e6817ac76f6cdd
Nadim Jahangir <nadim.jahangir@metafour.com>
2015-01-27 17:33:30 +0600

    WebAPI callback is refactored to take generic response object

--- app/src/main/java/com/metafour/cwbay/remote/WebAPI.java
+++ app/src/main/java/com/metafour/cwbay/remote/WebAPI.java
@@ -26,32 +26,19 @@ public class WebAPI {
 
     private static final String CATEGORY_VIEW_PATH = "/category/view/";
 
-    public static interface UserLoginCallback {
-        public void onUserLoginFailed(String reason);
-        public void onUserLoginSuccess(User user);
+    public static interface Callback<T> {
+        public void onFailed(String reason);
+        public void onSuccess(T responseObject);
     }
 
-    public static interface UserViewCallback {
-        public void onUserViewFailed(String reason);
-        public void onUserViewSuccess(User user);
-    }
-
-    public static interface UserEditCallback {
-        public void onUserEditFailed(String reason);
-        public void onUserEditSuccess(User user);
-    }
-
-    public static interface UserCreateCallback {
-        public void onUserCreateFailed(String reason);
-        public void onUserCreateSuccess(User user);
-    }
-
-    public static interface CategoryViewCallback {
-        public void onCategoryViewFailed(String reason);
-        public void onCategoryViewSuccess(Category category);
-    }
-
-    public static void userLogin(final Context context, final UserLoginCallback callback, final String email, final String pass) {
+    /**
+     * Web API method to login using user email and password.
+     * @param context App context
+     * @param callback User login callback
+     * @param email User email
+     * @param pass User password
+     */
+    public static void userLogin(final Context context, final Callback<User> callback, final String email, final String pass) {
         WebConnection.getInstance().setCredentials(email, pass);
         String encodedEmail = email;
         try {
@@ -64,16 +51,16 @@ public class WebAPI {
                 if (callback != null) {
                     switch (response.getStatus()) {
                         case WebConnection.Status.NOT_FOUND:
-                            callback.onUserLoginFailed(context.getResources().getString(R.string.login_invalid_cred));
+                            callback.onFailed(context.getResources().getString(R.string.login_invalid_cred));
                             break;
                         case WebConnection.Status.UNAUTHORIZED:
-                            callback.onUserLoginFailed(context.getResources().getString(R.string.login_invalid_cred));
+                            callback.onFailed(context.getResources().getString(R.string.login_invalid_cred));
                             break;
                         case WebConnection.Status.OK:
-                            callback.onUserLoginSuccess(UserBuilder.build(response.getContent()));
+                            callback.onSuccess(UserBuilder.build(response.getContent()));
                             break;
                         default:
-                            callback.onUserLoginFailed(context.getResources().getString(R.string.net_err));
+                            callback.onFailed(context.getResources().getString(R.string.net_err));
                             break;
                     }
 
@@ -83,7 +70,7 @@ public class WebAPI {
         USER_LOGIN_PATH + encodedEmail);
     }
 
-    public static void userView(final Context context, final UserViewCallback callback, final String email) {
+    public static void userView(final Context context, final Callback<User> callback, final String email) {
         String encodedEmail = email;
         try {
             URLEncoder.encode(encodedEmail, URL_ENCODING);
@@ -95,10 +82,10 @@ public class WebAPI {
                 if (callback != null) {
                     switch (response.getStatus()) {
                         case WebConnection.Status.OK:
-                            callback.onUserViewSuccess(UserBuilder.build(response.getContent()));
+                            callback.onSuccess(UserBuilder.build(response.getContent()));
                             break;
                         default:
-                            callback.onUserViewFailed(context.getResources().getString(R.string.net_err));
+                            callback.onFailed(context.getResources().getString(R.string.net_err));
                             break;
                     }
 
@@ -108,7 +95,7 @@ public class WebAPI {
         USER_VIEW_PATH + encodedEmail);
     }
 
-    public static void userEdit(final Context context, final UserEditCallback callback, final String email, final User user) {
+    public static void userEdit(final Context context, final Callback<User> callback, final String email, final User user) {
         String encodedEmail = email;
         try {
             URLEncoder.encode(encodedEmail, URL_ENCODING);
@@ -120,10 +107,10 @@ public class WebAPI {
                 if (callback != null) {
                     switch (response.getStatus()) {
                         case WebConnection.Status.OK:
-                            callback.onUserEditSuccess(UserBuilder.build(response.getContent()));
+                            callback.onSuccess(UserBuilder.build(response.getContent()));
                             break;
                         default:
-                            callback.onUserEditFailed(response.getContent());
+                            callback.onFailed(response.getContent());
                             break;
                     }
 
@@ -133,7 +120,7 @@ public class WebAPI {
         USER_EDIT_PATH + encodedEmail, UserBuilder.serialize(user));
     }
 
-    public static void userCreate(final Context context, final UserCreateCallback callback, final User user) {
+    public static void userCreate(final Context context, final Callback<User> callback, final User user) {
         WebConnection.getInstance().request(new WebConnection.Callback() {
             @Override
             public void onResponse(WebConnection.Response response) {
@@ -141,13 +128,13 @@ public class WebAPI {
                     Log.i(Constants.ACTIVITY_LOG_TAG, response.getStatus() + "");
                     switch (response.getStatus()) {
                         case WebConnection.Status.EXISTS:
-                            callback.onUserCreateFailed("Alreay exist");
+                            callback.onFailed("Alreay exist");
                             break;
                         case WebConnection.Status.OK:
-                            callback.onUserCreateSuccess(UserBuilder.build(response.getContent()));
+                            callback.onSuccess(UserBuilder.build(response.getContent()));
                             break;
                         default:
-                            callback.onUserCreateFailed(context.getResources().getString(R.string.net_err));
+                            callback.onFailed(context.getResources().getString(R.string.net_err));
                             break;
                     }
 
@@ -157,17 +144,18 @@ public class WebAPI {
         USER_CREATE_PATH, UserBuilder.serialize(user));
     }
 
-    public static void categoryView(final Context context, final CategoryViewCallback callback, final int id) {
+
+    public static void categoryView(final Context context, final Callback<Category> callback, final int id) {
         WebConnection.getInstance().request(new WebConnection.Callback() {
             @Override
             public void onResponse(WebConnection.Response response) {
                 if (callback != null) {
                     switch (response.getStatus()) {
                         case WebConnection.Status.OK:
-                            callback.onCategoryViewSuccess(CategoryBuilder.build(response.getContent()));
+                            callback.onSuccess(CategoryBuilder.build(response.getContent()));
                             break;
                         default:
-                            callback.onCategoryViewFailed(context.getResources().getString(R.string.net_err));
+                            callback.onFailed(context.getResources().getString(R.string.net_err));
                             break;
                     }
 
