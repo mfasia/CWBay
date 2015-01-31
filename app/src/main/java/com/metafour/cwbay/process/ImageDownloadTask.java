0027802ef8829c25e5d896753cdaf0773228b10a
nadim-jahangir-m4 <nadim.jahangir@metafour.com>
2015-01-31 11:53:45 +0600

    image resize added

--- app/src/main/java/com/metafour/cwbay/process/ImageDownloadTask.java
+++ app/src/main/java/com/metafour/cwbay/process/ImageDownloadTask.java
@@ -3,9 +3,13 @@ package com.metafour.cwbay.process;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
+
+import com.metafour.cwbay.util.Constants;
+import com.metafour.cwbay.util.Utility;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -50,8 +54,45 @@ public class ImageDownloadTask extends AsyncTask<String, Void, Bitmap> {
 
     @Override
     protected void onPostExecute(final Bitmap result) {
-        if(result != null){
-            imageView.setImageBitmap(result);
+        if(result != null) {
+            int iw = result.getWidth();
+            int ih = result.getHeight();
+
+            int vw = imageView.getWidth();
+            int vh = imageView.getHeight();
+
+            if (iw == vw && ih == vh) { // no need to scale
+                imageView.setImageBitmap(result);
+            } else if (ih > 220) {
+                if (iw < vw && ih < vh) {
+                    double r = (double) vh / ih;
+                    iw = (int) (r * iw);
+                    ih = vh;
+                }
+
+                if (ih > vh) {
+                    double r = (double) vh / ih;
+                    iw = (int) (r * iw);
+                    ih = vh;
+                }
+
+                if (iw > vw) {
+                    double r = (double) vw / iw;
+                    ih = (int) (r * ih);
+                    iw = vw;
+                }
+
+                Bitmap scaledResult = Bitmap.createScaledBitmap(result, iw, ih, false);
+                Bitmap bg = Bitmap.createBitmap(vw, vh, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bg);
+                canvas.drawBitmap(scaledResult, (vw - iw) / 2, (vh - ih) / 2, null);
+                imageView.setImageBitmap(bg);
+            } else {
+                imageView.setImageBitmap(result);
+            }
+
+
+
         }
     }
 }
