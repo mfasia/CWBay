f67aada543a3200f3cca8ee4e550bdb587c13c59
noor-siddique-m4 <noor.siddique@metafour.com>
2015-01-31 10:09:46 +0600

    merged

--- app/src/main/java/com/metafour/cwbay/util/Utility.java
+++ app/src/main/java/com/metafour/cwbay/util/Utility.java
@@ -5,6 +5,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
